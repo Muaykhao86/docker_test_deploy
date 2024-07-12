4f088e988cbcd0b12d550fc7bda565ba3d266431
dan-ntegra <daniel.hardiman@digital.mod.uk>
2024-07-12 17:21:05 +0100

    test-commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end('Hello, World! This is a simple Node.js server.');
 }
 
 const server = http.createServer(requestListener);
