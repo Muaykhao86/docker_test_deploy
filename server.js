3b783bc0c040194c6dd61e4bc0df17d421d385f1
Daniel Hardiman <38153501+Muaykhao86@users.noreply.github.com>
2024-08-22 15:15:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ const os = require('os');
 
 const interfaces = os.networkInterfaces();
 
-
 const requestListener = function (req, res) {
-  let ipAddress;
+  let ipAddresses = [];
 
   for (const interfaceName in interfaces) {
     for (const iface of interfaces[interfaceName]) {
       if (iface.family === 'IPv4' && !iface.internal) {
-        ipAddress = iface.address;
-        break;
+        ipAddresses.push(`Interface: ${interfaceName}, Address: ${iface.address}`);
       }
     }
-    if (ipAddress) break;
   }
 
-  res.writeHead(200);
-  res.end(`Hello, World! This is a simple Node.js server running on ${ipAddress} \n ${interfaces} \n`);
+  const responseText = `Hello, World! This is a simple Node.js server.\n\nIP Addresses:\n${ipAddresses.join('\n')}\n`;
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(responseText);
 }
 
 const server = http.createServer(requestListener);
